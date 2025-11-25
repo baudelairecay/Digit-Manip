6fdb0066d47ef2c147c16214b1caae46702c30c4
baudelairecay <156119001+baudelairecay@users.noreply.github.com>
2025-11-25 13:17:39 -0500

    Update Digmanip.h

--- Digmanip.h
+++ Digmanip.h
@@ -1,1 +1,9 @@
+#include <vector>
+#include <cmath>
 
+
+void print(std::vector <int> arr);
+int length(int num);
+int extract_digit(int num);
+std::vector<int> square_vector(std::vector<int> arr);
+std::vector<int> extract_digits(std::vector<int> arr);
